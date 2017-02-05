c77cbbcf86be898fa1e9b761213d74ae9bd63d5c
Christopher Lamont <chris_lamont@hotmail.com>
2017-02-05 14:33:57 +0100

    About screen added

--- XabluApp.Droid/Resources/Resource.designer.cs
+++ XabluApp.Droid/Resources/Resource.designer.cs
@@ -4088,8 +4088,8 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b0010
 			public const int MvxBindingTagUnique = 2131427344;
 			
-			// aapt resource value: 0x7f0b008b
-			public const int action0 = 2131427467;
+			// aapt resource value: 0x7f0b008c
+			public const int action0 = 2131427468;
 			
 			// aapt resource value: 0x7f0b0066
 			public const int action_bar = 2131427430;
@@ -4112,17 +4112,17 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b0043
 			public const int action_bar_title = 2131427395;
 			
-			// aapt resource value: 0x7f0b0088
-			public const int action_container = 2131427464;
+			// aapt resource value: 0x7f0b0089
+			public const int action_container = 2131427465;
 			
 			// aapt resource value: 0x7f0b0067
 			public const int action_context_bar = 2131427431;
 			
-			// aapt resource value: 0x7f0b008f
-			public const int action_divider = 2131427471;
+			// aapt resource value: 0x7f0b0090
+			public const int action_divider = 2131427472;
 			
-			// aapt resource value: 0x7f0b0089
-			public const int action_image = 2131427465;
+			// aapt resource value: 0x7f0b008a
+			public const int action_image = 2131427466;
 			
 			// aapt resource value: 0x7f0b0005
 			public const int action_menu_divider = 2131427333;
@@ -4139,11 +4139,11 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b0045
 			public const int action_mode_close_button = 2131427397;
 			
-			// aapt resource value: 0x7f0b008a
-			public const int action_text = 2131427466;
+			// aapt resource value: 0x7f0b008b
+			public const int action_text = 2131427467;
 			
-			// aapt resource value: 0x7f0b0098
-			public const int actions = 2131427480;
+			// aapt resource value: 0x7f0b0099
+			public const int actions = 2131427481;
 			
 			// aapt resource value: 0x7f0b0046
 			public const int activity_chooser_view_content = 2131427398;
@@ -4175,8 +4175,8 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b004d
 			public const int buttonPanel = 2131427405;
 			
-			// aapt resource value: 0x7f0b008c
-			public const int cancel_action = 2131427468;
+			// aapt resource value: 0x7f0b008d
+			public const int cancel_action = 2131427469;
 			
 			// aapt resource value: 0x7f0b0018
 			public const int center = 2131427352;
@@ -4190,8 +4190,8 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b005d
 			public const int checkbox = 2131427421;
 			
-			// aapt resource value: 0x7f0b0094
-			public const int chronometer = 2131427476;
+			// aapt resource value: 0x7f0b0095
+			public const int chronometer = 2131427477;
 			
 			// aapt resource value: 0x7f0b0024
 			public const int clip_horizontal = 2131427364;
@@ -4250,8 +4250,8 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b001b
 			public const int end = 2131427355;
 			
-			// aapt resource value: 0x7f0b009e
-			public const int end_padder = 2131427486;
+			// aapt resource value: 0x7f0b009f
+			public const int end_padder = 2131427487;
 			
 			// aapt resource value: 0x7f0b0011
 			public const int enterAlways = 2131427345;
@@ -4292,8 +4292,8 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b0078
 			public const int icon_container = 2131427448;
 			
-			// aapt resource value: 0x7f0b0099
-			public const int icon_group = 2131427481;
+			// aapt resource value: 0x7f0b009a
+			public const int icon_group = 2131427482;
 			
 			// aapt resource value: 0x7f0b0040
 			public const int ifRoom = 2131427392;
@@ -4301,8 +4301,8 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b0048
 			public const int image = 2131427400;
 			
-			// aapt resource value: 0x7f0b0095
-			public const int info = 2131427477;
+			// aapt resource value: 0x7f0b0096
+			public const int info = 2131427478;
 			
 			// aapt resource value: 0x7f0b000c
 			public const int item_touch_helper_previous_elevation = 2131427340;
@@ -4316,11 +4316,11 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b0077
 			public const int left_drawer = 2131427447;
 			
-			// aapt resource value: 0x7f0b009a
-			public const int line1 = 2131427482;
+			// aapt resource value: 0x7f0b009b
+			public const int line1 = 2131427483;
 			
-			// aapt resource value: 0x7f0b009c
-			public const int line3 = 2131427484;
+			// aapt resource value: 0x7f0b009d
+			public const int line3 = 2131427485;
 			
 			// aapt resource value: 0x7f0b0032
 			public const int listMode = 2131427378;
@@ -4328,11 +4328,11 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b004a
 			public const int list_item = 2131427402;
 			
-			// aapt resource value: 0x7f0b00a0
-			public const int masked = 2131427488;
+			// aapt resource value: 0x7f0b00a1
+			public const int masked = 2131427489;
 			
-			// aapt resource value: 0x7f0b008e
-			public const int media_actions = 2131427470;
+			// aapt resource value: 0x7f0b008f
+			public const int media_actions = 2131427471;
 			
 			// aapt resource value: 0x7f0b003d
 			public const int middle = 2131427389;
@@ -4355,14 +4355,14 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b002a
 			public const int normal = 2131427370;
 			
-			// aapt resource value: 0x7f0b0097
-			public const int notification_background = 2131427479;
+			// aapt resource value: 0x7f0b0098
+			public const int notification_background = 2131427480;
+			
+			// aapt resource value: 0x7f0b0092
+			public const int notification_main_column = 2131427474;
 			
 			// aapt resource value: 0x7f0b0091
-			public const int notification_main_column = 2131427473;
-			
-			// aapt resource value: 0x7f0b0090
-			public const int notification_main_column_container = 2131427472;
+			public const int notification_main_column_container = 2131427473;
 			
 			// aapt resource value: 0x7f0b0022
 			public const int parallax = 2131427362;
@@ -4385,11 +4385,11 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b001e
 			public const int right = 2131427358;
 			
-			// aapt resource value: 0x7f0b0096
-			public const int right_icon = 2131427478;
+			// aapt resource value: 0x7f0b0097
+			public const int right_icon = 2131427479;
 			
-			// aapt resource value: 0x7f0b0092
-			public const int right_side = 2131427474;
+			// aapt resource value: 0x7f0b0093
+			public const int right_side = 2131427475;
 			
 			// aapt resource value: 0x7f0b002e
 			public const int screen = 2131427374;
@@ -4484,8 +4484,8 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b001f
 			public const int start = 2131427359;
 			
-			// aapt resource value: 0x7f0b008d
-			public const int status_bar_latest_event_content = 2131427469;
+			// aapt resource value: 0x7f0b008e
+			public const int status_bar_latest_event_content = 2131427470;
 			
 			// aapt resource value: 0x7f0b0060
 			public const int submenuarrow = 2131427424;
@@ -4496,11 +4496,11 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b0033
 			public const int tabMode = 2131427379;
 			
-			// aapt resource value: 0x7f0b009d
-			public const int text = 2131427485;
+			// aapt resource value: 0x7f0b009e
+			public const int text = 2131427486;
 			
-			// aapt resource value: 0x7f0b009b
-			public const int text2 = 2131427483;
+			// aapt resource value: 0x7f0b009c
+			public const int text2 = 2131427484;
 			
 			// aapt resource value: 0x7f0b0054
 			public const int textSpacerNoButtons = 2131427412;
@@ -4517,8 +4517,8 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b0001
 			public const int textinput_error = 2131427329;
 			
-			// aapt resource value: 0x7f0b0093
-			public const int time = 2131427475;
+			// aapt resource value: 0x7f0b0094
+			public const int time = 2131427476;
 			
 			// aapt resource value: 0x7f0b004c
 			public const int title = 2131427404;
@@ -4556,8 +4556,11 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f0b0002
 			public const int view_offset_helper = 2131427330;
 			
-			// aapt resource value: 0x7f0b009f
-			public const int visible = 2131427487;
+			// aapt resource value: 0x7f0b00a0
+			public const int visible = 2131427488;
+			
+			// aapt resource value: 0x7f0b0088
+			public const int webview = 2131427464;
 			
 			// aapt resource value: 0x7f0b0042
 			public const int withText = 2131427394;
