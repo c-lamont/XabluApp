3c81ebefe363c92fa7e903dd06536d426a687a45
Christopher Lamont <chris_lamont@hotmail.com>
2017-02-09 20:56:01 +0100

    Blog images added

--- XabluApp.Droid/Resources/Resource.designer.cs
+++ XabluApp.Droid/Resources/Resource.designer.cs
@@ -3988,26 +3988,26 @@ namespace XabluApp.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020072
-			public const int avd_hide_password_1 = 2130837618;
+			// aapt resource value: 0x7f02007a
+			public const int avd_hide_password_1 = 2130837626;
 			
-			// aapt resource value: 0x7f020073
-			public const int avd_hide_password_2 = 2130837619;
+			// aapt resource value: 0x7f02007b
+			public const int avd_hide_password_2 = 2130837627;
 			
-			// aapt resource value: 0x7f020074
-			public const int avd_hide_password_3 = 2130837620;
+			// aapt resource value: 0x7f02007c
+			public const int avd_hide_password_3 = 2130837628;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020075
-			public const int avd_show_password_1 = 2130837621;
+			// aapt resource value: 0x7f02007d
+			public const int avd_show_password_1 = 2130837629;
 			
-			// aapt resource value: 0x7f020076
-			public const int avd_show_password_2 = 2130837622;
+			// aapt resource value: 0x7f02007e
+			public const int avd_show_password_2 = 2130837630;
 			
-			// aapt resource value: 0x7f020077
-			public const int avd_show_password_3 = 2130837623;
+			// aapt resource value: 0x7f02007f
+			public const int avd_show_password_3 = 2130837631;
 			
 			// aapt resource value: 0x7f020055
 			public const int Chris = 2130837589;
@@ -4016,85 +4016,109 @@ namespace XabluApp.Droid
 			public const int Coen = 2130837590;
 			
 			// aapt resource value: 0x7f020057
-			public const int design_bottom_navigation_item_background = 2130837591;
+			public const int corerelease = 2130837591;
 			
 			// aapt resource value: 0x7f020058
-			public const int design_fab_background = 2130837592;
+			public const int design_bottom_navigation_item_background = 2130837592;
 			
 			// aapt resource value: 0x7f020059
-			public const int design_ic_visibility = 2130837593;
+			public const int design_fab_background = 2130837593;
 			
 			// aapt resource value: 0x7f02005a
-			public const int design_ic_visibility_off = 2130837594;
+			public const int design_ic_visibility = 2130837594;
 			
 			// aapt resource value: 0x7f02005b
-			public const int design_password_eye = 2130837595;
+			public const int design_ic_visibility_off = 2130837595;
 			
 			// aapt resource value: 0x7f02005c
-			public const int design_snackbar_background = 2130837596;
+			public const int design_password_eye = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int Edwin = 2130837597;
+			public const int design_snackbar_background = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int Eric = 2130837598;
+			public const int developertools = 2130837598;
 			
 			// aapt resource value: 0x7f02005f
-			public const int Harry = 2130837599;
+			public const int Edwin = 2130837599;
 			
 			// aapt resource value: 0x7f020060
-			public const int Marc = 2130837600;
+			public const int Eric = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int Martijn = 2130837601;
+			public const int Harry = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int Maurits = 2130837602;
+			public const int Marc = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int navigation_empty_icon = 2130837603;
+			public const int Martijn = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int notification_action_background = 2130837604;
+			public const int Maurits = 2130837604;
 			
 			// aapt resource value: 0x7f020065
-			public const int notification_bg = 2130837605;
+			public const int navigation_empty_icon = 2130837605;
 			
 			// aapt resource value: 0x7f020066
-			public const int notification_bg_low = 2130837606;
+			public const int netstandard = 2130837606;
 			
 			// aapt resource value: 0x7f020067
-			public const int notification_bg_low_normal = 2130837607;
+			public const int notification_action_background = 2130837607;
 			
 			// aapt resource value: 0x7f020068
-			public const int notification_bg_low_pressed = 2130837608;
+			public const int notification_bg = 2130837608;
 			
 			// aapt resource value: 0x7f020069
-			public const int notification_bg_normal = 2130837609;
+			public const int notification_bg_low = 2130837609;
 			
 			// aapt resource value: 0x7f02006a
-			public const int notification_bg_normal_pressed = 2130837610;
+			public const int notification_bg_low_normal = 2130837610;
 			
 			// aapt resource value: 0x7f02006b
-			public const int notification_icon_background = 2130837611;
-			
-			// aapt resource value: 0x7f020070
-			public const int notification_template_icon_bg = 2130837616;
-			
-			// aapt resource value: 0x7f020071
-			public const int notification_template_icon_low_bg = 2130837617;
+			public const int notification_bg_low_pressed = 2130837611;
 			
 			// aapt resource value: 0x7f02006c
-			public const int notification_tile_bg = 2130837612;
+			public const int notification_bg_normal = 2130837612;
 			
 			// aapt resource value: 0x7f02006d
-			public const int notify_panel_notification_icon_bg = 2130837613;
+			public const int notification_bg_normal_pressed = 2130837613;
 			
 			// aapt resource value: 0x7f02006e
-			public const int Tirza = 2130837614;
+			public const int notification_icon_background = 2130837614;
+			
+			// aapt resource value: 0x7f020078
+			public const int notification_template_icon_bg = 2130837624;
+			
+			// aapt resource value: 0x7f020079
+			public const int notification_template_icon_low_bg = 2130837625;
 			
 			// aapt resource value: 0x7f02006f
-			public const int xablu_logo = 2130837615;
+			public const int notification_tile_bg = 2130837615;
+			
+			// aapt resource value: 0x7f020070
+			public const int notify_panel_notification_icon_bg = 2130837616;
+			
+			// aapt resource value: 0x7f020071
+			public const int partnersummit = 2130837617;
+			
+			// aapt resource value: 0x7f020072
+			public const int porttoxamarin = 2130837618;
+			
+			// aapt resource value: 0x7f020073
+			public const int techdays = 2130837619;
+			
+			// aapt resource value: 0x7f020074
+			public const int Tirza = 2130837620;
+			
+			// aapt resource value: 0x7f020075
+			public const int uiuxreview = 2130837621;
+			
+			// aapt resource value: 0x7f020076
+			public const int xablu_logo = 2130837622;
+			
+			// aapt resource value: 0x7f020077
+			public const int xablupremierpartner = 2130837623;
 			
 			static Drawable()
 			{
